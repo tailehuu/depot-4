aa0c2d49fa9cf7f92ec8153c0e5e7fe1d0a31229
Tai LE <lhtai181@gmail.com>
2017-06-06 15:32:08 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot4::Application.config.secret_key_base = '865418fbc7617931b8a707b6e95d28a5fa190f6f83d6abdc26f2bbeefe943f07c183cbcc1243d336d21efe78da8fc928210b37e4c3eaf2272925de37d413bd83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Depot4::Application.config.session_store :cookie_store, key: '_depot-4_session'
